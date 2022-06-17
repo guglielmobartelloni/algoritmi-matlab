a5e0230fcc3830e640457704c04a7212473e2e21
samoorai <hirotakatech@gmail.com>
2022-06-17 14:39:23 +0200

    Migliorata formattazione hermite

--- Hermite.m
+++ Hermite.m
@@ -1,22 +1,24 @@
 function y = Hermite(X, Y, Y1, XQ)
     %
-    %   y = newton(X,Y,XQ);
+    %   y = Hermite(X, Y, Y1, XQ)
     %   Calcolo del polinomio interpolante nella forma di Newton
     %   Input:
     %   (X,Y): dati del problema
     %   XQ: vettore in cui calcolare il polinomio
     %
-    %if length(X)~= length(Y)
-    %   error('Errore nei dati del problema');
-    %end
+    if length(X) ~= length(Y)
+        error('Errore nei dati del problema');
+    end
+
     if length(X) ~= length(unique(X))
         error('Le ascisse devono essere distinte')
     end
+
     % creazione del vettore con alternate le Y con le derivate prime
-    appo= zeros(1,length(a1)*2);
-    appo(1:2:end-1) = Y;
-    appo(2:2:end)   = Y1;
-    Y=appo;
+    appo = zeros(1, length(Y) * 2);
+    appo(1:2:end - 1) = Y;
+    appo(2:2:end) = Y1;
+    Y = appo;
 
     % Calcolo delle differenze divise di Hermite
     df = divdifH(X, Y);
